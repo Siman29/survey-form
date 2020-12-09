181ba440bf215cb7a41ee77cc63b02023817e69f
Siman29 <60322023+Siman29@users.noreply.github.com>
2020-12-09 14:20:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function value_checks() {
-    var num_value = document.getElementsByClassName("number");
-    if (num_value.value !== Number) {
-        alert("Enter a number ...Not any text");
-        num_value.style.borderColor = 'red';
-    }
-}
